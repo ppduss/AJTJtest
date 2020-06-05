32a64eb2ca7662f634751a4a8b2bf2b76ab0a7ad
Patrick <ppduss@gmail.com>
2020-06-05 10:06:01 -0400

    test

---
+++ precache-manifest.b75efa980a7614bfe9fe8b5f8a151647.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd7ece186c4387ae1777852de0142ac",
+    "url": "/ylcQuizzes/static/media/dark_flake.5fd7ece1.png"
+  },
+  {
+    "revision": "85fa8846a6559845e1ce",
+    "url": "/ylcQuizzes/static/css/main.53823ebc.chunk.css"
+  },
+  {
+    "revision": "f758ab9b25dc513b874640aabe12edf5",
+    "url": "/ylcQuizzes/static/media/back-arrow-grey.f758ab9b.svg"
+  },
+  {
+    "revision": "699d041f51f5039c11a5",
+    "url": "/ylcQuizzes/static/js/1.699d041f.chunk.js"
+  },
+  {
+    "revision": "ec8c45d95d0225a597ea",
+    "url": "/ylcQuizzes/static/js/runtime~main.ec8c45d9.js"
+  },
+  {
+    "revision": "85fa8846a6559845e1ce",
+    "url": "/ylcQuizzes/static/js/main.85fa8846.chunk.js"
+  },
+  {
+    "revision": "40a9174fe2f6ff5bcf400bdb4a83507b",
+    "url": "/ylcQuizzes/static/media/small_milk.40a9174f.png"
+  },
+  {
+    "revision": "746269efdf553df1dc0397930a8da18a",
+    "url": "/ylcQuizzes/static/media/big_milk.746269ef.png"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/ylcQuizzes/static/media/baseline-menu-24px.c275e518.svg"
+  },
+  {
+    "revision": "699d041f51f5039c11a5",
+    "url": "/ylcQuizzes/static/css/1.4196ce62.chunk.css"
+  },
+  {
+    "revision": "42b68434fc91d32168358e3f582eeee9",
+    "url": "/ylcQuizzes/index.html"
+  }
+];
